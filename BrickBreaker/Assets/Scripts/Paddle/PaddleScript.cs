695cbcac9561c6f7ce85860abca2d66c8d69838d
ManuelVassallo <56382051+ManuelVassallo@users.noreply.github.com>
2019-12-26 12:53:36 +0100

    Paddle Movement
    Paddle Movement and Screen Coardinates

---
+++ BrickBreaker/Assets/Scripts/Paddle/PaddleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    public float speed;
+    public float rightScreenEdge;
+    public float leftScreenEdge;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //paddle movement and paddle movement speed
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+        //check the left screen coardiantes to see if the paddle goes out of screen
+        if(transform.position.x < leftScreenEdge)
+        {
+            transform.position = new Vector2(leftScreenEdge, transform.position.y);
+        }
+        //check the right screen coardiantes to see if the paddle goes out of screen
+        if (transform.position.x > rightScreenEdge)
+        {
+            transform.position = new Vector2(rightScreenEdge, transform.position.y);
+        }
+    }
+}
